e94e501035ecb0295f8762af8755defed7ed7f10
toshi <toshi.sakao.r@gmail.com>
2023-02-14 11:50:31 +0100

    made the index file generator and started with token

--- konkordans/src/compiler.rs
+++ konkordans/src/compiler.rs
@@ -1,15 +1,24 @@
 use encoding_rs::WINDOWS_1252;
 use encoding_rs_io::DecodeReaderBytesBuilder;
-use std::fs::{OpenOptions, File};
+use std::fs::{File, OpenOptions};
 use std::io::{self, BufRead, BufReader, Read, Write};
 use std::thread::current;
+use std::{fmt, path::Path};
 
 const KORPUS_FILE: &str = "files/korpus";
 const TOKEN_FILE: &str = "files/token.txt";
 
-pub fn read_token() -> Vec<Vec<(String, u64)>> {
-    let mut tokens: Vec<Vec<(String, u64)>> = Vec::with_capacity(1000);
+pub fn read_token() {
+    if !Path::new("files/index").exists() {
+        construct_index_file();
+    } else {
+        println!("already there");
+    }
 
+    construct_magic_file();
+}
+
+fn construct_index_file() {
     let mut buf = BufReader::new(
         DecodeReaderBytesBuilder::new()
             .encoding(Some(WINDOWS_1252))
@@ -20,15 +29,63 @@ pub fn read_token() -> Vec<Vec<(String, u64)>> {
 
     let mut line = String::new();
     let mut bytes: usize = buf.read_line(&mut line).unwrap();
-    let mut last_key = String::new();
+    let mut last_key: String = String::new();
+    // let mut current_byte: usize = 0;
+    let mut index = String::new();
+
+    while bytes > 0 {
+        let key = line.split_whitespace().next().unwrap();
+        let byte_offset = line.split_whitespace().last().unwrap();
+
+        if last_key != key {
+            index += "\n";
+            index += key;
+        }
+        index += " ";
+        index += byte_offset;
+
+        last_key = key.to_string();
+        line.clear();
+        bytes = buf.read_line(&mut line).unwrap();
+        // let key = line.split_whitespace().next().unwrap();
+        // let hash = hash_three(key);
+
+        // if tokens.len() <= hash as usize {
+        //     tokens.resize(hash + 1, Vec::new());
+        // }
+        // if last_key != key {
+        //     tokens[hash as usize].push((key.to_string(), current_byte as u64));
+        // }
+
+        // last_key = key.to_string();
+        // current_byte += bytes;
+
+        // line.clear();
+        // bytes = buf.read_line(&mut line).unwrap();
+    }
+
+    let mut file = File::create("files/index").expect("coudnt make index file");
+    file.write_all(index.as_bytes());
+
+    println!("yey");
+}
+
+fn construct_magic_file() -> Vec<Vec<(String, u64)>> {
+    let mut tokens: Vec<Vec<(String, u64)>> = Vec::with_capacity(1000);
+    let mut buf = BufReader::new(File::open("files/index").unwrap());
+
+    let mut line = String::new();
+    let mut bytes: usize = buf.read_line(&mut line).unwrap();
+    let mut last_key: String = String::new();
     let mut current_byte: usize = 0;
 
+    bytes = buf.read_line(&mut line).unwrap();
     while bytes > 0 {
         let key = line.split_whitespace().next().unwrap();
         let hash = hash_three(key);
 
         if tokens.len() <= hash as usize {
-            tokens.resize(hash+1, Vec::new());
+            tokens.resize(hash + 1, Vec::new());
         }
         if last_key != key {
             tokens[hash as usize].push((key.to_string(), current_byte as u64));
@@ -40,12 +97,8 @@ pub fn read_token() -> Vec<Vec<(String, u64)>> {
         line.clear();
         bytes = buf.read_line(&mut line).unwrap();
     }
-    println!("yey");
+
     tokens
-}
-
-fn construct_magic_file(tokens: Vec<Vec<(String, u64)>>) {
-
 }
 
 fn hash_three(word: &str) -> usize {
--- konkordans/src/main.rs
+++ konkordans/src/main.rs
@@ -2,8 +2,12 @@ use crate::compiler::read_token;
 
 mod compiler;
 
+use std::time;
+
 fn main() {
     println!("Hello, world!");
-    let token = read_token();
+    let start = time::Instant::now();
+    read_token();
 
+    println!("COMPILING COMPLETE\nTook: {} seconds", start.elapsed().as_secs());
 }
